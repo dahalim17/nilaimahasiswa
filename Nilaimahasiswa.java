89da85b43987e1679edf3fc26b7e5c7eb3a61b3e
beningkarina <57781942+beningkarina@users.noreply.github.com>
2020-01-12 20:17:49 +0700

    nings

---
+++ Nilaimahasiswa.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nilaimahasiswa;
+
+/**
+ *
+ * @author ASUS ROG
+ */
+public class Nilaimahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int bhs_indo=80;
+        int bhs_ingris=75;
+        double mtk=78.98;
+        int agama=90;  
+        double total_nilai=bhs_indo+bhs_ingris+mtk+agama;  
+        System.out.print("Total niali:" +total_nilai);
+    }   
+}
+    
